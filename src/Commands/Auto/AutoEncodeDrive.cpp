d26d9caaf2ab0bb6ae472847b1bff3dd79ce1886
RandyNgo02 <35178937+RandyNgo02@users.noreply.github.com>
2018-10-12 15:39:24 -0600

    Add files via upload

--- src/Commands/Auto/AutoEncodeDrive.cpp
+++ src/Commands/Auto/AutoEncodeDrive.cpp
@@ -1,38 +1,44 @@
-//#include <Commands/Auto/AutoTimedDriveForward/AutoTimedDriveForwardCommandGroups/AutoTimedDriveForwardCommands/AutoTimedDriveForwardDriveWheels.h>
-#include <Commands/Auto/AutoTimedDrive.h>
+#include "AutoEncodeDrive.h"
 #include "../../Robot.h"
+#include <SmartDashboard/SmartDashboard.h>
 
-AutoTimedDrive::AutoTimedDrive() {
+AutoEncodeDrive::AutoEncodeDrive() {
 	// Use Requires() here to declare subsystem dependencies
-	// eg. Requires(&Robot::chassis);
+	// eg. Requires(Robot::chassis.get());
 	Requires(&Robot::drivetrain);
 	timer = new frc::Timer();
 }
 
 // Called just before this Command runs the first time
-void AutoTimedDrive::Initialize() {
+void AutoEncodeDrive::Initialize() {
+	Robot::drivetrain.ConfigureEncoders();
+	Robot::drivetrain.ResetEncodersandNavX();
 	timer->Start();
 }
 
 // Called repeatedly when this Command is scheduled to run
-void AutoTimedDrive::Execute() {
+void AutoEncodeDrive::Execute() {
 	Robot::drivetrain.TankDrive(-0.5, -0.5);
+	frc::SmartDashboard::PutNumber("Auto Timer", timer->Get());
 }
 
 // Make this return true when this Command no longer needs to run execute()
-bool AutoTimedDrive::IsFinished() {
-	if (timer->Get()>=2)
+bool AutoEncodeDrive::IsFinished() {
+	if (Robot::drivetrain.ReturnDrivenInches(3) >= 109)
 	{
+		timer->Stop();
 		return true;
 	}
 	return false;
 }
 
 // Called once after isFinished returns true
-void AutoTimedDrive::End() {
+void AutoEncodeDrive::End() {
 	Robot::drivetrain.TankDrive(0, 0);
 }
 
 // Called when another command which requires one or more of the same
 // subsystems is scheduled to run
-void AutoTimedDrive::Interrupted() {}
+void AutoEncodeDrive::Interrupted() {
+
+}
